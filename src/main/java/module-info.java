9e83b25a8b0a6d13a65be1777002e00b9c0029b9
C-VD <vldr@disroot.org>
2023-08-23 13:22:22 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.thefinish {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.thefinish to javafx.fxml;
+    exports com.example.thefinish;
+}
